1d2c7ce1669632a341dd4b34574a237f59c8c579
sp2aplicacion <sp2aplicacion@outlook.com>
2015-04-20 22:53:39 -0500

    Delete

--- sp2_TESIS/.bash_history
+++
@@ -1,9 +0,0 @@
-cd 
-pwd
-ls
-cd public_html/
-ls
-cd persistencia/
-ls
-more Conexion.php 
-exit
--- sp2_TESIS/public_html/scripts/validarContra.js
+++
@@ -1,7 +0,0 @@
-verificar(obj)
-	{
-	if(obj.cant.value==''||obj.cnueva.value==''||obj.cconf.value=='') 
-		return 'Debe llenar todos los campos';
-	if(obj.cconf.value!=obj.cnueva.value)
-		return 'La contraseña nueva y la confirmacion deben ser iguales';
-	}
